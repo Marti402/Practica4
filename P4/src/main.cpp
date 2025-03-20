eafcdc67b54bacb5099a555fcb55cace804d1f16
Marti004 <160530055+Marti402@users.noreply.github.com>
2025-03-20 18:08:33 +0100

    Add files via upload

---
+++ P4/src/main.cpp
@@ -0,0 +1,97 @@
+/*
+
+#include<Arduino.h>
+
+void anotherTask(void *parameter);
+
+void setup() {
+  Serial.begin(112500);
+
+  xTaskCreate(
+      anotherTask,     
+      "another Task",  
+      10000,           
+      NULL,             
+      1,               
+      NULL              
+  );
+}
+
+void loop() {
+  Serial.println("this is ESP32 Task");
+  delay(1000);
+}
+
+void anotherTask(void *parameter) {
+  
+  for (;;) {
+    Serial.println("this is another Task");
+    delay(1000);
+  }
+
+  vTaskDelete(NULL);
+}
+
+*/
+
+#include <Arduino.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+
+
+#define Pulsador2 2 //Pulsador 1
+#define LED_ROJO 47  // LED rojo en el pin 47
+#define LED_VERDE 39 // LED verde en el pin 39
+#define PULSADOR 48  // Pulsador en el pin 46
+SemaphoreHandle_t xSemaphore;
+
+void tareaEncenderRojo(void *pvParameters) {
+    while (1) {
+        if (digitalRead(PULSADOR) == LOW and digitalRead(Pulsador2) == LOW) { // Pulsador presionado (asumiendo pull-up interno)
+            xSemaphoreTake(xSemaphore, portMAX_DELAY);
+            digitalWrite(LED_ROJO, HIGH);
+            Serial.println("LED Rojo Encendido");
+            vTaskDelay(pdMS_TO_TICKS(1000));
+            digitalWrite(LED_ROJO, LOW);
+            Serial.println("LED Rojo Apagado");
+            xSemaphoreGive(xSemaphore);
+        }else{Serial.println("AA");}
+        vTaskDelay(pdMS_TO_TICKS(50)); // Pequeña espera para evitar rebotes
+    }
+}
+
+void tareaEncenderVerde(void *pvParameters) {
+    while (1) {
+        xSemaphoreTake(xSemaphore, portMAX_DELAY);
+        digitalWrite(LED_VERDE, HIGH);
+        Serial.println("LED Verde Encendido");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        digitalWrite(LED_VERDE, LOW);
+        Serial.println("LED Verde Apagado");
+        xSemaphoreGive(xSemaphore);
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(LED_ROJO, OUTPUT);
+    pinMode(LED_VERDE, OUTPUT);
+    pinMode(PULSADOR, INPUT_PULLUP); // Configura el pulsador con resistencia pull-up interna
+
+    xSemaphore = xSemaphoreCreateBinary();
+    if (xSemaphore == NULL) {
+        Serial.println("Error creando el semáforo");
+        return;
+    }
+
+    xSemaphoreGive(xSemaphore);
+
+    xTaskCreate(tareaEncenderRojo, "Tarea Encender Rojo", 4096, NULL, 1, NULL);
+    xTaskCreate(tareaEncenderVerde, "Tarea Encender Verde", 4096, NULL, 1, NULL);
+}
+
+void loop() {
+    // No hacer nada en loop, las tareas de FreeRTOS manejan el flujo
+}
